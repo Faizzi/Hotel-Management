cf7a6fbc578e2cf848e77da2c6f5d4e8999bfa01
Faizan Ullah <93101478+Faizzi@users.noreply.github.com>
2024-02-14 11:01:54 +0500

    Created pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-out' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='text-red-600 h1'>SignUp</div>
+  )
+}
+
+export default SignUp
