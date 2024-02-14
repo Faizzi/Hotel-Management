a83750a99238d24361193c6835aad4f57f153703
Faizan Ullah <93101478+Faizzi@users.noreply.github.com>
2024-02-14 11:30:52 +0500

    created Header

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
+import Header from './components/Header'
+import About from './pages/About'
 const App = () => {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
         <Route path='/sign-in' element={<SignIn/>} />
         <Route path='/sign-out' element={<SignUp/>}/>
         <Route path='/profile' element={<Profile/>}/>
